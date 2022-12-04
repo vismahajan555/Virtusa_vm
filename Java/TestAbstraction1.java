eec32753c28bae662eb2ed82ea121492fd723c91
Mahajan Vaishnavi Vivek <106020355+vismahajan555@users.noreply.github.com>
2022-12-04 13:38:06 +0530

    Add files via upload

---
+++ Java/TestAbstraction1.java
@@ -0,0 +1,24 @@
+//Abstract Class Example
+abstract class Shape{  
+    abstract void draw();  
+}  
+
+class Rectangle extends Shape{  
+    void draw(){
+        System.out.println("Drawing Rectangle");
+    }  
+}  
+class Circle1 extends Shape{  
+    void draw(){
+        System.out.println("Drawing Circle");
+    }  
+}  
+
+class TestAbstraction1{  
+    public static void main(String args[]){  
+        Shape s=new Circle1();
+        s.draw();
+        s = new Rectangle();
+        s.draw();  
+    }  
+}  
